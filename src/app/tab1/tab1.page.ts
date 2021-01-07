bd7aad3541e14625867eb9c004bdaef5a39b8ec9
Edwin-Antonio <edanarellanoma@ittepic.edu.mx>
2021-01-07 09:53:20 -0800

    V0.1.0

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { Estudiante } from '../models/estudiante';
+import { EstudianteService } from '../services/estudiante.service';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit{
 
-  constructor() {}
+  public myForm: FormGroup;
+  public student: Estudiante;
+  constructor(private studentService: EstudianteService, private fb: FormBuilder) { }
+
+
+  ngOnInit() {
+    this.myForm = this.fb.group(
+      {
+        name: [''],
+        controlnumber: [''],
+        curp: [''],
+        age: [0],
+        active: [false]
+      });
+  }
+
+  create(){
+    this.student = {
+      name: this.myForm.controls.name.value,
+      controlnumber: this.myForm.controls.controlnumber.value,
+      curp: this.myForm.controls.curp.value,
+      age: this.myForm.controls.age.value,
+      active: this.myForm.controls.active.value
+    };
+    this.studentService.createStudent(this.student);
+  }
 
 }
